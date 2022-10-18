875f0181a1cc4b12cb54770f2d3d03ed8e557bb9
Rutvik2699 <rutvikbhoj2611@gmail.com>
2022-10-18 11:57:09 -0400

    Promises chain .then

--- logic.js
+++ logic.js
@@ -17,7 +17,7 @@
 // console.log("End");
 
 
-// =====================================================
+// ==============================================================================================
 //Callbacks
 // function loadScript(src,Callback){
 //     var script = document.createElement("script");
@@ -48,7 +48,7 @@
 
 //Video number 53
 //Callback Hell OR Pyramid of Doom
-//
+//===============================================================================================
 
 //Video number 54
 //Intro to promises
@@ -66,36 +66,86 @@
 // console.log("My name is "+"John")
 // console.log(promise)
 
-
+//==============================================================================================
 //Video number 55
 //Promise .then() and .catch()
 
-let p1 = new Promise((resolve,reject)=>{
-    console.log("promise is pending")
-    setTimeout(()=>{
-      //  console.log("I am a promise and I am resolved")
-        resolve(true)
-    },5000)
-})
+// let p1 = new Promise((resolve,reject)=>{
+//     console.log("promise is pending")
+//     setTimeout(()=>{
+//       //  console.log("I am a promise and I am resolved")
+//         resolve(true)
+//     },5000)
+// })
 
-let p2 = new Promise((resolve,reject)=>{
-    console.log("promise is pending")
-    setTimeout(()=>{
-       // console.log("I am a promise and I am rejected")
-        reject(new Error("I am an error"))
-    },5000)
-})
-p1.then((value)=>{
-    console.log(value)
-})
-
-// p2.catch((error)=>{
+// let p2 = new Promise((resolve,reject)=>{
+//     console.log("promise is pending")
+//     setTimeout(()=>{
+//        // console.log("I am a promise and I am rejected")
+//         reject(new Error("I am an error"))
+//     },5000)
+// })
+// p1.then((value)=>{
 //     console.log(value)
 // })
 
+// // p2.catch((error)=>{
+// //     console.log(value)
+// // })
 
-p2.then((value)=>{
+
+// p2.then((value)=>{
+//     console.log(value)
+// },(error)=>{
+//     console.log(error)
+// })
+
+//===============================================================================================
+//Video 56 
+//Promise Chaining .then() calls
+
+// let p1 = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//     console.log("Resolved after 2 secs")
+//     resolve(56)
+// },2000)
+// })
+
+// p1.then((value)=>{
+//     console.log(value)
+//     let p2 = new Promise((resolve,reject)=>{
+//         setTimeout(()=>{ resolve("Promise 2")},2000)
+       
+//     })
+//     return p2;
+// }).then((value)=>{
+//     console.log("we are done")
+//     return 2
+// }).then((value)=>{
+//     console.log("Now we are pakka done")
+// })
+
+
+
+
+const loadScript =(src)=>{
+    return new Promise((resolve,reject)=>{
+    let script=document.createElement("script")
+    script.type="text/javascript"
+    script.src=src
+    document.body.appendChild(script)
+    script.onload=()=>{
+            resolve(1)
+    } 
+    script.onerror=()=>{reject(0)}
+})
+}
+
+let p1 = loadScript("https://www.youtbe.com/c/CodeWithHarry/videos") 
+p1.then((value)=>{
     console.log(value)
-},(error)=>{
-    console.log(error)
+}).then((value)=>{
+    console.log("Second script ready")
+}).catch((error)=>{
+    console.log("we are sorry but we are having problem loading the script")
 })
