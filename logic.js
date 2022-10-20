73d6146d678d782b2f5b7a0bdf728ca279f4c0d9
Rutvik2699 <rutvikbhoj2611@gmail.com>
2022-10-20 18:47:00 -0400

    POST Request

--- logic.js
+++ logic.js
@@ -322,12 +322,46 @@
 //=====================================================================================
 //Video 66 Fetch API
 
-let p = fetch("https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js")
-p.then((response)=>{
-    console.log(response.status)
-    console.log(response.ok)
-    console.log(response.headers)
-    return response.json()
-}).then((value2)=>{
-    console.log(value2)
-})
+// let p = fetch("https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js")
+// p.then((response)=>{
+//     console.log(response.status)
+//     console.log(response.ok)
+//     console.log(response.headers)
+//     return response.json()
+// }).then((value2)=>{
+//     console.log(value2)
+// })
+
+
+//Video 67 POST Request
+const createTodo = async (todo) => {
+    let options = {
+            method: "POST",
+            headers: {
+                    "Content-type": "application/json"
+            },
+            body: JSON.stringify(todo),
+    }
+    let p = await fetch('https://jsonplaceholder.typicode.com/posts', options)
+    let response = await p.json()
+    return response
+}
+
+const getTodo = async (id) => {
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts/' + id)
+    let r = await response.json()
+    return r
+}
+
+const mainFunc = async () => {
+    let todo = {
+            title: 'Harry2',
+            body: 'bhai2',
+            userId: 1100,
+    }
+    let todor = await createTodo(todo)
+    console.log(todor)
+    console.log(await getTodo(101))
+}
+
+mainFunc()
