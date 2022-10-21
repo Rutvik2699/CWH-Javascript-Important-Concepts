4e0976d48e0dc4bd2a9f1b4c80a66b02aee99033
Rutvik2699 <rutvikbhoj2611@gmail.com>
2022-10-21 10:53:32 -0400

    Cookies

--- logic.js
+++ logic.js
@@ -332,36 +332,53 @@
 //     console.log(value2)
 // })
 
+//===============================================================================
+// //Video 67 POST Request
+// const createTodo = async (todo) => {
+//     let options = {
+//             method: "POST",
+//             headers: {
+//                     "Content-type": "application/json"
+//             },
+//             body: JSON.stringify(todo),
+//     }
+//     let p = await fetch('https://jsonplaceholder.typicode.com/posts', options)
+//     let response = await p.json()
+//     return response
+// }
 
-//Video 67 POST Request
-const createTodo = async (todo) => {
-    let options = {
-            method: "POST",
-            headers: {
-                    "Content-type": "application/json"
-            },
-            body: JSON.stringify(todo),
-    }
-    let p = await fetch('https://jsonplaceholder.typicode.com/posts', options)
-    let response = await p.json()
-    return response
-}
+// const getTodo = async (id) => {
+//     let response = await fetch('https://jsonplaceholder.typicode.com/posts/' + id)
+//     let r = await response.json()
+//     return r
+// }
 
-const getTodo = async (id) => {
-    let response = await fetch('https://jsonplaceholder.typicode.com/posts/' + id)
-    let r = await response.json()
-    return r
-}
+// const mainFunc = async () => {
+//     let todo = {
+//             title: 'Harry2',
+//             body: 'bhai2',
+//             userId: 1100,
+//     }
+//     let todor = await createTodo(todo)
+//     console.log(todor)
+//     console.log(await getTodo(101))
+// }
 
-const mainFunc = async () => {
-    let todo = {
-            title: 'Harry2',
-            body: 'bhai2',
-            userId: 1100,
-    }
-    let todor = await createTodo(todo)
-    console.log(todor)
-    console.log(await getTodo(101))
-}
+// mainFunc()
 
-mainFunc()
+//===============================================================================
+
+//Video 68 Cooookies
+//Cookeis are small strings of data stored directly into the browser
+
+
+console.log(document.cookie)
+document.cookie = "name=harry1122334400"
+document.cookie = "name2=harry11223344002"
+document.cookie = "name2=harry"
+let key = prompt("Enter yoru key")
+let value = prompt("Enter yoru value")
+document.cookie = `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
+
+//console.log(document.cookie)
+console.log(decodeURIComponent(document.cookie))
